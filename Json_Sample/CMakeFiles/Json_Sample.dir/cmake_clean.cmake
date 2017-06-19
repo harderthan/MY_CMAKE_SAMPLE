eecf4470ea842badd8ca5bf4e36b66d3944342f9
KwangHoe Kim <kheo1772@gmail.com>
2017-06-20 00:06:51 +0900

    *first commit

---
+++ Json_Sample/CMakeFiles/Json_Sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Json_Sample.dir/main.cpp.o"
+  "Json_Sample.pdb"
+  "Json_Sample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Json_Sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
